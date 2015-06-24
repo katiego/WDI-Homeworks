8a112aaebc124f6d6e5b1a62adb199162cb632e6
Katie Gaudin <katiegaudin2@GaudinMacintosh.local>
2015-06-24 15:54:27 -0700

    added login file

---
+++ login.js
@@ -0,0 +1,16 @@
+var userLogin = {
+    userName: "Katie",
+    userPassword: ""
+}
+
+if ((userLogin.userName) && (!userLogin.userPassword)) {
+    console.log("Enter password for user " + [userLogin.userName])
+}
+
+var userPasswordINPUT
+if (userPassword === userPasswordINPUT) {
+    console.log("Great!")
+}
+else {
+    console.log("alert")
+}
